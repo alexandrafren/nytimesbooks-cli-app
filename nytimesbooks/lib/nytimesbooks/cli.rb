755f7e95d9d5100136a195177de33d2739138d92
Alexandra Fren <alexandra.fren@gmail.com>
2018-07-08 19:09:36 +0000

    Improved flow of CLI

--- nytimesbooks/lib/nytimesbooks/cli.rb
+++ nytimesbooks/lib/nytimesbooks/cli.rb
@@ -8,24 +8,31 @@ class Nytimesbooks::CLI
   def categories
     puts "There are 5 main categories to choose from:"
     puts "1.Combined Print & E-Book Fiction 2. Hardcover Fiction 3.Combined Print & E-Book Nonfiction 4.Hardcover Nonfiction 5.Paperback Nonfiction"
-    puts "Which category would you like to explore further? Enter 1-5, or exit to leave the program."
-    input = gets.chomp
+    input = nil
+    while input != "exit"
+      puts "Which category would you like to explore further? Enter 1-5, or exit to leave the program."
+      input = gets.chomp
     case input
     when "1"
-      combined_fiction
+      puts "combined_fiction"
     when "2"
-      hardcover_fiction
+      puts "hardcover_fiction"
     when "3"
-      combined_nonfiction
+      puts "combined_nonfiction"
     when "4"
-      hardcover_nonfiction
+      puts "hardcover_nonfiction"
     when "5"
-      paperback_nonfiction
+      puts "paperback_nonfiction"
     when "exit"
       goodbye
     else
       puts "I don't understand your input. Please try again."
-
+    end
   end
+end
+
+    def goodbye
+      puts "Enjoy your reading!"
+    end
 
 end
