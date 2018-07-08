16345f4d6476091b13bf4b2ec37dc9463f8fc08f
Alexandra Fren <alexandra.fren@gmail.com>
2018-07-08 19:06:06 +0000

    Set up first level of CLI

--- nytimesbooks/lib/nytimesbooks/cli.rb
+++ nytimesbooks/lib/nytimesbooks/cli.rb
@@ -1,6 +1,31 @@
 class Nytimesbooks::CLI
 
   def call
+    puts "Welcome to the New York Times Best Selling Books list!"
+    categories
+  end
+
+  def categories
+    puts "There are 5 main categories to choose from:"
+    puts "1.Combined Print & E-Book Fiction 2. Hardcover Fiction 3.Combined Print & E-Book Nonfiction 4.Hardcover Nonfiction 5.Paperback Nonfiction"
+    puts "Which category would you like to explore further? Enter 1-5, or exit to leave the program."
+    input = gets.chomp
+    case input
+    when "1"
+      combined_fiction
+    when "2"
+      hardcover_fiction
+    when "3"
+      combined_nonfiction
+    when "4"
+      hardcover_nonfiction
+    when "5"
+      paperback_nonfiction
+    when "exit"
+      goodbye
+    else
+      puts "I don't understand your input. Please try again."
+
   end
 
 end
