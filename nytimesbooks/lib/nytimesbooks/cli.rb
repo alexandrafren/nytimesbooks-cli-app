f61bf7238f9ef191aac39fe075b1f6d417197c92
Alexandra Fren <alexandra.fren@gmail.com>
2018-07-09 16:24:02 +0000

    Adding in fiction menu

--- nytimesbooks/lib/nytimesbooks/cli.rb
+++ nytimesbooks/lib/nytimesbooks/cli.rb
@@ -2,10 +2,10 @@ class Nytimesbooks::CLI
 
   def call
     puts "Welcome to the New York Times Best Selling Books list!"
-    maincategories
+    main_categories
   end
 
-  def categories
+  def main_categories
     puts "Are you interested in seeing Fiction, Nonfiction, or Childrens Best Sellers lists? (Type exit to leave program)"
     input = nil
     while input != "exit"
@@ -24,6 +24,33 @@ class Nytimesbooks::CLI
     end
   end
 end
+
+  def fiction_menu
+    puts "There are 3 Fiction categories: 1. Combined Print & E-Book Fiction 2. Hardcover Fiction 3. Paperback Trade Fiction"
+    puts "Which are you interested in? Type 1-3 or exit to leave the program."
+      input = gets.chomp.downcase
+    case input
+    when "1"
+      bookarray = Nytimesbooks::Book.combined_fiction
+      bookarray.each_with_index do |book, i|
+        puts "#{i+1}. #{book.name} #{book.author}"
+      end
+      goodbye
+    when "2"
+      puts "nonfiction_menu"
+    when "3"
+      puts "childrens_menu"
+    when "exit"
+      goodbye
+    else
+      puts "I don't understand your input. Please try again."
+  end
+end
+
+
+
+
+
 
    #def combined_fiction
      #bookarray = Nytimesbooks::Book.combined_fiction
