cab7a146a54b7d0149a5aab568a9bc38e2894c5e
Alexandra Fren <alexandra.fren@gmail.com>
2018-07-09 21:06:18 +0000

    main of CLI finished

--- nytimesbooks/lib/nytimesbooks/cli.rb
+++ nytimesbooks/lib/nytimesbooks/cli.rb
@@ -97,22 +97,26 @@ end
         @childrens_middle.each_with_index do |book, i|
           puts "#{i+1}. #{book.name} #{book.author}"
         end
+        childrens_middle
         goodbye
       when "2"
         @childrens_picture = Nytimesbooks::Book.childrens_picture
         @childrens_picture.each_with_index do |book, i|
           puts "#{i+1}. #{book.name} #{book.author}"
         end
+        childrens_picture
       when "3"
         @childrens_series = Nytimesbooks::Book.childrens_series
         @childrens_series.each_with_index do |book, i|
           puts "#{i+1}. #{book.name} #{book.author}"
         end
+        childrens_series
       when "4"
         @young_adult = Nytimesbooks::Book.young_adult
         @young_adult.each_with_index do |book, i|
           puts "#{i+1}. #{book.name} #{book.author}"
         end
+        young_adult
       when "exit"
         goodbye
       else
@@ -154,6 +158,30 @@ end
       puts "Which of these books would you like to see a description of? Enter 1 - 15, or exit to leave."
       input = gets.chomp.to_i
       puts "#{@paperback_nonfiction[input - 1].name} #{@paperback_nonfiction[input -1].author} #{@paperback_nonfiction[input - 1].bookdescription}"
+    end
+
+    def childrens_middle
+      puts "Which of these books would you like to see a description of? Enter 1 - 10, or exit to leave."
+      input = gets.chomp.to_i
+      puts "#{@childrens_middle[input - 1].name} #{@childrens_middle[input -1].author} #{@childrens_middle[input - 1].bookdescription}"
+    end
+
+    def childrens_picture
+      puts "Which of these books would you like to see a description of? Enter 1 - 10, or exit to leave."
+      input = gets.chomp.to_i
+      puts "#{@childrens_picture[input - 1].name} #{@childrens_picture[input -1].author} #{@childrens_picture[input - 1].bookdescription}"
+    end
+
+    def childrens_series
+      puts "Which of these books would you like to see a description of? Enter 1 - 10, or exit to leave."
+      input = gets.chomp.to_i
+      puts "#{@childrens_series[input - 1].name} #{@childrens_series[input -1].author} #{@childrens_series[input - 1].bookdescription}"
+    end
+
+    def young_adult
+      puts "Which of these books would you like to see a description of? Enter 1 - 10, or exit to leave."
+      input = gets.chomp.to_i
+      puts "#{@young_adult[input - 1].name} #{@young_adult[input -1].author} #{@young_adult[input - 1].bookdescription}"
     end
 
     def goodbye
