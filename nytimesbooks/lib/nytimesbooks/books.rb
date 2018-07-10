4d460cc007d8d4fa255eba1d25a64b27c67b3228
Alexandra Fren <alexandra.fren@gmail.com>
2018-07-10 17:48:19 +0000

    making bin.rb more DRY

--- nytimesbooks/lib/nytimesbooks/books.rb
+++ nytimesbooks/lib/nytimesbooks/books.rb
@@ -1,17 +1,31 @@
 class Nytimesbooks::Book
   attr_accessor :name, :author, :bookdescription
 
-  def self.combined_fiction
-    puts "Combined Print & E-Book Fiction"
-    doc = Nokogiri::HTML(open("https://www.nytimes.com/books/best-sellers/combined-print-and-e-book-fiction/?action=click&contentCollection=Books&referrer=https%3A%2F%2Fwww.nytimes.com%2Fsection%2Fbooks&region=Body&module=CompleteListLink&version=Fiction&pgtype=Reference"))
-    combined_fiction_array = []
+
+  def self.new_book(doc)
+    new_array = []
     doc.css("div.book-body").each do |book|
       new_book = self.new
       new_book.name = book.css("h2.title").text
       new_book.author = book.css("p.author").text
       new_book.bookdescription = book.css("p.description").text
-      combined_fiction_array << new_book
+      new_array << new_book
     end
+    new_array
+  end
+
+  def self.combined_fiction
+    puts "Combined Print & E-Book Fiction"
+    doc = Nokogiri::HTML(open("https://www.nytimes.com/books/best-sellers/combined-print-and-e-book-fiction/?action=click&contentCollection=Books&referrer=https%3A%2F%2Fwww.nytimes.com%2Fsection%2Fbooks&region=Body&module=CompleteListLink&version=Fiction&pgtype=Reference"))
+    combined_fiction_array = []
+    #doc.css("div.book-body").each do |book|
+      #new_book = self.new
+    #  new_book.name = book.css("h2.title").text
+      #new_book.author = book.css("p.author").text
+      #new_book.bookdescription = book.css("p.description").text
+      #book = new_book(doc)
+      combined_fiction_array = new_book(doc)
+    #end
     combined_fiction_array
   end
 
