f7540d44f266eca18e844d771c0ba196fc03d86e
Alexandra Fren <alexandra.fren@gmail.com>
2018-07-10 19:31:05 +0000

    now executable

--- nytimesbooks/lib/nytimesbooks.rb
+++ nytimesbooks/lib/nytimesbooks.rb
@@ -1,6 +1,6 @@
 require "open-uri"
 require "nokogiri"
 
-require_relative "./nytimesbooks/version"
-require_relative "./nytimesbooks/cli"
-require_relative "./nytimesbooks/books"
+require_relative "nytimesbooks/version"
+require_relative "nytimesbooks/cli"
+require_relative "nytimesbooks/books"
