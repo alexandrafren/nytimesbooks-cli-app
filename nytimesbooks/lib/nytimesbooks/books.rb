fd49df80e7f938b20d6c3317ea69ef99e3054aa1
Alexandra Fren <alexandra.fren@gmail.com>
2019-11-12 20:55:40 +0000

    correct nytimes new css

--- nytimesbooks/lib/nytimesbooks/books.rb
+++ nytimesbooks/lib/nytimesbooks/books.rb
@@ -4,11 +4,11 @@ class Nytimesbooks::Book
 
   def self.new_book(doc)
     new_array = []
-    doc.css("div.book-body").each do |book|
+    doc.css("#app #site-content ol li.css-13y32ub").each do |book|
       new_book = self.new
-      new_book.name = book.css("css-5pe77f").text
-      new_book.author = book.css("css-hjukut").text
-      new_book.bookdescription = book.css("css-14lubdp").text
+      new_book.name = book.css("h3.css-5pe77f").text
+      new_book.author = book.css("p.css-hjukut").text
+      new_book.bookdescription = book.css("p.css-14lubdp").text
       new_array << new_book
     end
     new_array
