cb295c45320898c57834e6c244b43720cda5ef05
alexandrafren <alexandra.fren@gmail.com>
2019-11-12 12:15:04 -0800

    update css for nytimesbooks

--- nytimesbooks/lib/nytimesbooks/books.rb
+++ nytimesbooks/lib/nytimesbooks/books.rb
@@ -6,9 +6,9 @@ class Nytimesbooks::Book
     new_array = []
     doc.css("div.book-body").each do |book|
       new_book = self.new
-      new_book.name = book.css("h2.title").text
-      new_book.author = book.css("p.author").text
-      new_book.bookdescription = book.css("p.description").text
+      new_book.name = book.css("css-5pe77f").text
+      new_book.author = book.css("css-hjukut").text
+      new_book.bookdescription = book.css("css-14lubdp").text
       new_array << new_book
     end
     new_array
